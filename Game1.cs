0f386cc630d2cc1bd24a7b9cdf7f06a5a11162c3
Ethan Jones <339467243@GoTVDSB.ca>
2024-04-18 12:53:09 -0400

    lesson 2 tutorial

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_Scaling_and_text
 {
@@ -8,12 +9,20 @@ namespace Monogame_Scaling_and_text
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Random genorator;
+        Texture2D rectangleTexture;
+        Texture2D circleTexture;
+
+        SpriteFont messageFont;
+
+        Rectangle redRect, greenRect;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
@@ -21,6 +30,9 @@ namespace Monogame_Scaling_and_text
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            genorator = new Random();
+            redRect = new Rectangle(genorator.Next(5,200), genorator.Next(5, 200), genorator.Next(5, 500), genorator.Next(5, 500));
+            greenRect = new Rectangle(genorator.Next(5, 200), genorator.Next(5, 200), genorator.Next(5, 500), genorator.Next(5, 500));
         }
 
         protected override void LoadContent()
@@ -28,6 +40,9 @@ namespace Monogame_Scaling_and_text
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+            circleTexture = Content.Load<Texture2D>("circle");
+            messageFont = Content.Load<SpriteFont>("Message Font");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +51,7 @@ namespace Monogame_Scaling_and_text
                 Exit();
 
             // TODO: Add your update logic here
+            
 
             base.Update(gameTime);
         }
@@ -45,7 +61,11 @@ namespace Monogame_Scaling_and_text
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(rectangleTexture, redRect, Color.Red);
+            _spriteBatch.Draw(circleTexture, greenRect, Color.Green);
+            _spriteBatch.DrawString(messageFont, "Hello World", new Vector2(10,350), Color.Cornsilk);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
